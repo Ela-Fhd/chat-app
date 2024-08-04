8a0e509e13fdda53f582a201931affc0661a9dd0
ela.fhd <elahe.fahmideh@gmail.com>
2024-08-04 12:21:48 +0330

    feat:create server and client

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { join, dirname } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+
+//? http server
+const server = createServer(app);
+const dirName = dirname(fileURLToPath(import.meta.url));
+const io = new Server(server);
+
+//! ==> / is a rout handler
+//* initialize app to be a function handler - we supply this app to http server
+app.get("/", (req, res) => {
+  res.sendFile(join(dirName, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+//* 3000 is port
+//! this is http server
+server.listen(3000, () => {
+  console.log("server running");
+});
